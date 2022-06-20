8d23b6c14d2f47ba533c96b57d197f15f21d4f07
Alex Hurley <the-Hull@users.noreply.github.com>
2022-06-20 17:01:57 +0200

    compose set up

---
+++ 06_week/02_project/00_pipe/tweet_collector/01_collect.py
@@ -0,0 +1,26 @@
+import tweepy
+import os
+# Here we need to import our credentials:
+# from credentials_anastasia import BEARER_TOKEN
+import logging
+
+BEARER_TOKEN = os.getenv("TWITTER_BEARER")
+
+
+print(BEARER_TOKEN[1:5])
+
+client = tweepy.Client(bearer_token=BEARER_TOKEN)
+
+if client:
+    logging.critical("\nAuthentication OK")
+else:
+    logging.critical('\nVerify your credentials')
+
+
+safiyanoble = client.get_user(username='safiyanoble', user_fields=['name', 'id', 'created_at'])
+user = safiyanoble.data
+
+logging.log(level=1, 
+    msg = f'The user with name {user.name} and ID {user.id} created their twitter account on {user.created_at}')
+
+print(f'The user with name {user.name} and ID {user.id} created their twitter account on {user.created_at}' )
