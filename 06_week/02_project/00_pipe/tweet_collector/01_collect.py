c75e45b740c50089a5c20aeb2db25a69024ad936
Alex Hurley <the-Hull@users.noreply.github.com>
2022-06-21 16:52:09 +0200

    tweets out

--- 06_week/02_project/00_pipe/tweet_collector/01_collect.py
+++ 06_week/02_project/00_pipe/tweet_collector/01_collect.py
@@ -1,8 +1,7 @@
 import tweepy
 import os
-# Here we need to import our credentials:
-# from credentials_anastasia import BEARER_TOKEN
 import logging
+import utils
 
 
 from datetime import datetime
@@ -16,7 +15,6 @@ logging.critical("\n" + current_time)
 BEARER_TOKEN = os.getenv("TWITTER_BEARER")
 
 
-print(BEARER_TOKEN[1:5])
 
 client = tweepy.Client(bearer_token=BEARER_TOKEN)
 
@@ -26,10 +24,46 @@ else:
     logging.critical('\nVerify your credentials')
 
 
-safiyanoble = client.get_user(username='safiyanoble', user_fields=['name', 'id', 'created_at'])
-user = safiyanoble.data
+########################
+# Get User Information #
+########################
 
-logging.log(level=1, 
-    msg = f'The user with name {user.name} and ID {user.id} created their twitter account on {user.created_at}')
+# https://docs.tweepy.org/en/stable/client.html#tweepy.Client.get_user
+# https://developer.twitter.com/en/docs/twitter-api/data-dictionary/object-model/user
 
-print(f'The user with name {user.name} and ID {user.id} created their twitter account on {user.created_at}' )
+response = client.get_user(
+    username='GalaxyKate',
+    user_fields=['created_at', 'description', 'location',
+                 'public_metrics', 'profile_image_url']
+)
+
+user = response.data
+
+print(dict(user))
+
+
+#########################
+# Get a user's timeline #
+#########################
+
+# https://docs.tweepy.org/en/stable/pagination.html#tweepy.Paginator
+# https://docs.tweepy.org/en/stable/client.html#tweepy.Client.get_users_tweets
+# https://developer.twitter.com/en/docs/twitter-api/data-dictionary/object-model/tweet
+
+cursor = tweepy.Paginator(
+    method=client.get_users_tweets,
+    id=user.id,
+    exclude=['replies', 'retweets'],
+    tweet_fields=['author_id', 'created_at', 'public_metrics'],
+
+).flatten(limit=20)
+
+
+tweetdfs = []
+for tweet in cursor:
+    
+    tweetdfs.append(utils.make_tweet_df(tweet))
+tweets = utils.pd.concat(tweetdfs, axis = 0)
+
+print(tweets)
+print(user.id)    
