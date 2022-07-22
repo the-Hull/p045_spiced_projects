580e698be52f1385b96713544a0089397086dd08
Alex Hurley <the-Hull@users.noreply.github.com>
2022-07-22 15:45:43 +0200

    bootstrap

--- 10_week/02_project/application.py
+++ 10_week/02_project/application.py
@@ -3,21 +3,41 @@ import src.utils as su
 import recommender as rc
 
 app = Flask(__name__)
-@app.route('/', methods=['GET', 'POST'])
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+
+
+
+#     # method_pred = None
+
+#     # if request.args.getlist("method")[0]
+
+
+
+#     return render_template('index.html', movies = rc.movies['title'], method_pred = None)
+
+@app.route('/', methods = ['GET', 'POST'])
 def index():
 
+    method_pred = None
+    if request.args.getlist("rec_method"):
+        method_pred = request.args.getlist("rec_method")[0]
+
+    print(method_pred)
+    render = render_template(
+        'index.html',
+        movies = rc.movies['title'], 
+        method_pred = method_pred)
+      
 
 
-    # method_pred = None
-
-    # if request.args.getlist("method")[0]
+    return render
 
 
-
-    return render_template('index.html', movies = rc.movies['title'])
 @app.route('/recommender/')
 def recommender():
     return render_template('recommender.html')
 if __name__ == '__main__':
     print(rc.movies.head())
     app.run(debug=True)
+    
